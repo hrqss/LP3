1ab29cfa47b0144310cc61e030aa7574093f0fda
02935249127 <02935249127@192.168.56.1>
2019-08-07 20:51:54 -0400

    updste codigo 2

--- BibliotecaVirtual/src/br/edu/ifms/biblioteca/model/Multa.java
+++ BibliotecaVirtual/src/br/edu/ifms/biblioteca/model/Multa.java
@@ -7,5 +7,31 @@ public class Multa {
 	private int idEmprestimo;
 	private LocalDate dtDevolucao;
 	private LocalDate valor; //nova data para fazer emprestimo
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdEmprestimo() {
+		return idEmprestimo;
+	}
+	public void setIdEmprestimo(int idEmprestimo) {
+		this.idEmprestimo = idEmprestimo;
+	}
+	public LocalDate getDtDevolucao() {
+		return dtDevolucao;
+	}
+	public void setDtDevolucao(LocalDate dtDevolucao) {
+		this.dtDevolucao = dtDevolucao;
+	}
+	public LocalDate getValor() {
+		return valor;
+	}
+	public void setValor(LocalDate valor) {
+		this.valor = valor;
+	}
+	
+	
 
 }
